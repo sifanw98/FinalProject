1cf8ffc85889a00ad9a89ddb7a1f97748c3de3d8
SophieJiang <qtlogresw@gmail.com>
2025-02-28 14:56:53 -0800

    Comment out Eureka/OpenFeign. Updated dependencies in pom.xml.

--- item-service/src/main/java/com/example/item_service/ItemServiceApplication.java
+++ item-service/src/main/java/com/example/item_service/ItemServiceApplication.java
@@ -2,10 +2,10 @@ package com.example.item_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 // @EnableFeignClients
+@SpringBootApplication
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
